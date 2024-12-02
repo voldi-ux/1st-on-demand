eec1e67ff5cd5a3a600a5a3e26e7f6c6402a5106
voldi-ux <itsvee156@gmail.com>
2024-12-02 03:22:12 +0200

    added a quick response code

---
+++ assets/js/tailwind_config.js
@@ -0,0 +1,19 @@
+/** @format */
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        main: "#2E3192",
+        second: "#00AEEF",
+        third: "#FBB040"
+      },
+      backgroundColor: {
+        // Custom background colors
+        "overlay": "rgba(0,0,0,0.7)",
+        "brand-orange": "#FF5722"
+      }
+    }
+  }
+};
